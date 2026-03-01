0a82b6015dd596b16b9873725118f709d66c4250
Nus <200475008+nus25@users.noreply.github.com>
2026-03-01 10:03:14 +0000

    perf: reduce object creation

--- packages/generator/src/endpoints/getDocument.ts
+++ packages/generator/src/endpoints/getDocument.ts
@@ -1,19 +1,18 @@
 import { DOCUMENT_TYPES } from 'shared/src/constants';
 import { NotFoundError } from 'shared/src/errors/core';
 
-// get service document from D1 documents table
+const SQL_SELECT_DOCUMENT = `
+SELECT url, content
+FROM documents
+WHERE type = ?
+LIMIT 1;`;
 
+// get service document from D1 documents table
 export async function getDocument(env: Env, type: string): Promise<Response> {
   if (!type || (type !== DOCUMENT_TYPES.PRIVACY_POLICY && type !== DOCUMENT_TYPES.TOS)) {
     throw new NotFoundError('Content not found');
   }
 
-  const SQL_SELECT_DOCUMENT = `
-        SELECT url, content
-        FROM documents
-        WHERE type = ?
-        LIMIT 1
-    `;
   const result = await env.DB.prepare(SQL_SELECT_DOCUMENT).bind(type).first<{
     url: string | null;
     content: string | null;
